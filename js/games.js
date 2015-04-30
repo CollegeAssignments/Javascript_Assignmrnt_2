ddb8339a1a1032d37e3e611b9cc807ddce1c2d39
Brian Geraghty <s00077881@mail.itsligo.ie>
2015-04-30 23:48:44 +0100

    Start Assignment
    Create & Shuufle Deck functions

---
+++ js/games.js
@@ -0,0 +1,46 @@
+/***************************************************
+PLAYING CARD FUNCTIONS
+****************************************************/
+
+//Create new deck of cards (non shufled)
+function CreateCardDeck()
+{
+    var cardDeck = [];
+    var suits = ["hearts","clubs","spades","diamonds"];
+    var cardValue = ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"];
+
+    for(var i=0;i<suits.length;i++)
+    {
+        for(var j=0;j<cardValue.length;j++)
+        {
+            var newCard = {
+                suit: suits[i],
+                value: cardValue[j]
+            };
+            cardDeck.push(newCard);
+        }
+    }
+    return cardDeck;
+}
+
+
+//Shufle Deck
+//Used this after my own version was very inefficent
+//http://bost.ocks.org/mike/shuffle/
+function ShuffleCardDeck(cardDeck)
+{
+    var m = cardDeck.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = cardDeck[m];
+        cardDeck[m] = cardDeck[i];
+        cardDeck[i] = t;
+    }
+    return cardDeck;
+}
